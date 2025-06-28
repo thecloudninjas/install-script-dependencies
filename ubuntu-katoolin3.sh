c0eb210016087143c99e5e305b30825e5fa06d62
Cloud Ninja <129507570+thecloudninjas@users.noreply.github.com>
2025-06-29 02:34:05 +0530

    Update ubuntu-katoolin3.sh

--- ubuntu-katoolin3.sh
+++ ubuntu-katoolin3.sh
@@ -91,10 +91,9 @@ terraform -version
 # --------------------
 echo "☁️ Installing AWS CLI v2..."
 cd /tmp
-curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip -q awscliv2.zip
-sudo ./aws/install
-rm -rf aws awscliv2.zip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install --update
 aws --version || echo "❌ AWS CLI install failed"
 
 # --------------------
